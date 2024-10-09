c240ffdee41a60aa4ec744be4facb98b808a888e
Nashrul <149583365+Nashrul235@users.noreply.github.com>
2024-10-09 22:07:19 +0800

    Delete POSTTEST5 directory

--- POSTTEST5/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'koneksi.php';
-$db="db_helm";
-$id = $_GET['id'];
-$query = "DELETE FROM helm WHERE id_helm = $id";
-mysqli_query($koneksi, $query);
-
-header('Location: CRUD.php');
-?>
